5cf02bc838fc2dad9e94e1bc1cdb5b61d0bcb28d
jinken87 <ken87208@gmail.com>
2023-12-15 16:11:43 +0800

    fix (vite.config.js) : 補上 base /layout/
    需求:
    
    1. 調整config.js
    
    調整:
    
    1. 調整完成

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.s461qera2ig"
+    "revision": "0.ol22701r0fg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/layout/",
   plugins: [
     vue(),
     vueJsx(),
